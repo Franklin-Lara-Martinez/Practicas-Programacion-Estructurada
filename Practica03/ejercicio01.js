dea0558503d1b28e76456dffaf2d6fb988c4ff1d
Frank007 <jl724505@gmail.com>
2026-09-08 19:57:36 -0600

    Tercera practica de laboratorio03

---
+++ Practica03/ejercicio01.js
@@ -0,0 +1,22 @@
+// Validar número positivo: Solicite un número al usuario. 
+// Verifique si es positivo, negativo o cero, y muestre un mensaje correspondiente.
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Ingrese un numero: " , function(num){
+
+    if(num >=1){
+        console.log(`su numero es positivo`);
+    }else if(num -=1){
+              console.log(`su numero es negativo`);
+    }else{
+                  console.log(`su numero es 0`);
+            
+    }
+    rl.close();
+})
---
+++ Practica03/ejercicio04.js
@@ -0,0 +1,26 @@
+// Categoría por edad: Solicite la edad del usuario. 
+// Clasifíquelo en: niño (0-12), adolescente (13-17), adulto (18-64) o adulto mayor (65+).
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Ingrese su edad: " , function(edad){
+
+    if(edad >= 0 && edad <= 12){
+        console.log(`Su categoria es: niño`);
+    }else if(edad >= 13 && edad <= 17){
+        console.log(`Su categoria es: adolescente`);
+    }else if(edad >= 18 && edad <= 64){
+        console.log(`Su categoria es: adulto`);
+    }else if(edad >= 65){
+        console.log(`Su categoria es: adulto mayor`);
+    }else{
+        console.log(`Edad no valida`);
+    }
+    rl.close();
+
+});
